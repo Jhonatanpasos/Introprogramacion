fe8d211c3c233f837ffc3ada6e2645f549bfdd41
Jhonatanpasos <jhonatanpasos2013@gmail.com>
2021-02-15 22:07:19 -0500

    Actualizado

--- Clases/Clase2.py
+++ Clases/Clase2.py
@@ -1,23 +1,33 @@
+#Otorgamos un valor verdadero o falso a las variables
 pruebaV = True
 pruebaF = False
 print (pruebaV)
 print (pruebaF)
 pruebaV = pruebaF
 print (pruebaV)
+#Le damos valores a las variables para la actividad
 edad = 19
 estatura = 1.72
 peso = 45
 NOMBRE = "Jhonatan Alexander Pasos Cortes"
+#Separamos la primera sección para observar condicionales
 print ("#"*10, "Mayoria de edad", "#"*10)
+#Miramos si la edad introducida es mayor o igual a 18 
 isMayorEdad = edad >= 18
 print (isMayorEdad)
+#Separamos la segunda sección para observar condicionales
 print ("#"*10, "Estatura bajo el promedio", "#"*10)
+#Miramos si la estatura es menor de 1.70
 isMayorEstatura = estatura < 1.70
 print (isMayorEstatura)
+#Separamos la tercera sección para observar condicionales
 print ("#"*10, "peso diferente de 84", "#"*10)
+#Miramos si tiene un peso diferente a 84 kg
 isPesoDiferente = peso != 84
 print (isPesoDiferente)
+#Separamos la cuarta sección para observar condicionales
 print ("#"*10, "Esta el apellido en nombre", "#"*10)
+#Miramos si el apellido "Pasos" esta en la varible nombre
 apellido = "Pasos"
 isApellido = apellido in NOMBRE
 print (isApellido)
--- Clases/Operaciones.py
+++ Clases/Operaciones.py
@@ -1,13 +1,20 @@
+#Le otorgamos un valor a las variables
 numeroA = 87
 numeroB = 83
+#Realizamos una suma entre las variables
 sumar = numeroA + numeroB
 print ("el resutado de la suma es", sumar)
 print (f"la suma dio {sumar} exitosamente")
+#Realizamos una resta entre las variables
 restar = numeroA - numeroB
 print ("el resutado de la resta es", restar)
+#Realizamos una multiplicación entre las variables
 multiplicar =numeroA * numeroB
 print ("el resultado de la multiplicación es", multiplicar)
+#Realizamos una división entre variables
 dividir = numeroA / numeroB
 print ("el resultado de la división es", dividir)
+#Elevamos una variable a otra variable 
 exponente = numeroA**numeroB
 print ("el resultado de la elevación al exponente es", exponente)
+#Usamos print para mostrar en pantalla los resultados de las operaciones
