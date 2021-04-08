5794e8845f9fe54efb497ffb6a6a0202eb6ee18e
jhonatanpasos <jhonatanpasos2013@gmail.com>
2021-04-08 09:04:12 -0500

    Punto 5 parcial

--- Examenes/Parcial/Punto5.py
+++ Examenes/Parcial/Punto5.py
@@ -3,3 +3,15 @@
 # valor. Por ejemplo, si el usuario digita 3 se mostrará 1 y si el usuario digita 5 mostrará 3 si 
 # ingresa 100 debe mostrar el número que le corresponda en la sucesión
 
+def Fibonacci (sucesión):
+    a = 0
+    b = 1
+    for sucesión in range (sucesión -1):
+        c = a + b
+        a = b
+        b = c
+    return (a)
+
+Lista = [0,1,1,2,3,5,8,13,21,34,55,89,144,...]
+numero = Fibonacci (5)
+print (f"El numero correspondiente de la sucesión con la posición indicada es {numero}")
