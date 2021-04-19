38b8b16d47e897d5ccac797bf4369d2aba7830cd
jhonatanpasos <jhonatanpasos2013@gmail.com>
2021-04-19 12:21:07 -0500

    Clases y objetos

---
+++ Clases/Clases y objetos/Utilizandoclases.py
@@ -0,0 +1,7 @@
+import Introducción as moldes
+
+biomedicio = moldes.Biomedico('Jhonatan',20,1.63,'Biomédicina')
+biomedicio.mostrarAtributos()
+biomedicio.recorrerDistancia(27)
+biomedicio.solucionarProblemas('Alta demanda en equipos médicos extranjeros')
+biomedicio.crearAlgoritmo('Detector de picos ECG')
