a4da31f39bfcc92f97ba2414c8bf9ac8fe3b576c
jhonatanpasos <jhonatanpasos2013@gmail.com>
2021-04-20 08:00:58 -0500

    Taller Herencia

---
+++ Talleres/TallerHerencia.py
@@ -0,0 +1,19 @@
+# 1. Cree la clase Persona con id, nombre, edad y cree la función hablar la cual dado mensaje se muestra 
+#    en pantalla y cree la clase caminar que dado una cantidad de pasos muestra en pantalla cuanto ha caminado
+
+
+
+# 2. Herede la clase Persona y cree la clase Doctor el cual tendrá el nuevo atributo de especialidad y podrá 
+#    ejecutar una nueva función, la cual es dado una enfermedad muestre en pantalla: procedo a tratar dicha enfermedad
+
+
+
+# 3. Herede la clase Persona y cree la clase Nutricionista y cree un atributo que se refiera a la universidad 
+#     en la que fue egresado. También una función que devuelva el IMC dado el peso y altura de un paciente
+
+
+
+# 4. Herede la clase Persona y cree la clase Abogado adicione dos atributos uno asociado a su especialidad y 
+#    el otro a la universidad de la que egresó. Finalmente cree la función que dado un nombre y el caso de cliente 
+#    el abogado diga : procedo a representar al cliente {nombre} en el caso {caso}
+
