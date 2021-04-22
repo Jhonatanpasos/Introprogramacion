e9e79575fbd31325055a0fc5619d9b7f22b95812
jhonatanpasos <jhonatanpasos2013@gmail.com>
2021-04-22 00:53:53 -0500

    Taller Herencia

--- Talleres/TallerHerencia.py
+++ Talleres/TallerHerencia.py
@@ -36,7 +36,21 @@ Jhonatan.tratamiento ("Aterosclerosis")
 # 3. Herede la clase Persona y cree la clase Nutricionista y cree un atributo que se refiera a la universidad 
 #     en la que fue egresado. También una función que devuelva el IMC dado el peso y altura de un paciente
 
+class Nutricionista(Persona):
+    def __init__ (self, nombreEntrada, edadEntrada, idEntrada, universidadEntrada):
+        Persona.__init__(self, nombreEntrada, edadEntrada, idEntrada)
+        self.universidad = universidadEntrada
+        
+    def IMC (self, pesoEntrada, alturaEntrada):
+        self.peso = pesoEntrada
+        self.altura = alturaEntrada
+        IMC = self.peso /(self.altura**2)
+        print (f"""Hola, mi nombre es {self.nombre} y soy nutricionista egresado de la universidad {self.universidad}
+            De acuerdo a tus registro, tu peso es {self.peso} kg y tu altura es {self.altura} cm
+            Tus resultado del indice de masa corporal es {IMC}""")
 
+Jhonatan = Nutricionista ("Jhonatan", 19, 1000938372, "CES")
+Jhonatan.IMC (45, 1.74,)
 
 # 4. Herede la clase Persona y cree la clase Abogado adicione dos atributos uno asociado a su especialidad y 
 #    el otro a la universidad de la que egresó. Finalmente cree la función que dado un nombre y el caso de cliente 
